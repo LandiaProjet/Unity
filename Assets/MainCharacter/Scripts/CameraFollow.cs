90922a3808579ebb11d86d83bfacde3d23c055f7
Kerlann <kerlann.pro@gmail.com>
2022-01-09 19:26:44 +0100

    update

--- Assets/MainCharacter/Scripts/CameraFollow.cs
+++ Assets/MainCharacter/Scripts/CameraFollow.cs
@@ -8,15 +8,8 @@ public class CameraFollow : MonoBehaviour
 
     private Vector3 velocity;
 
-    public Vector3 minValues, maxValue;
-
     void Update()
     {
-        Vector3 boundPosition = new Vector3(
-            Mathf.Clamp(player.transform.position.x, minValues.x, maxValue.x),
-            Mathf.Clamp(player.transform.position.y, minValues.y, maxValue.y),
-            Mathf.Clamp(player.transform.position.z, minValues.z, maxValue.z));
-
         transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + posOffset, ref velocity, timeOffset); 
     }
 }
