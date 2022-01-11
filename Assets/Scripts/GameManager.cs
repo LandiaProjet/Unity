57538ca3e15e9b6fbf50537a51bedce90d4c4052
Kerlann <kerlann.pro@gmail.com>
2022-01-11 23:39:46 +0100

    GameManager HudManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,8 +8,14 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public GameObject[] dontDestroyOnLoad;
 
+    public static HudManager hudManager = new HudManager();
+
     private void Awake() {
         instance = this;
+        //Set les valeurs par default
+        hudManager.reset();
+
+
         DontDestroyOnLoad(gameObject);
         foreach(var obj in dontDestroyOnLoad){
             DontDestroyOnLoad(obj);
