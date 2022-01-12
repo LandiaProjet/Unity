254eb419a312b7bf70a4991019287b0cd224c9a0
Kerlann <kerlann.pro@gmail.com>
2022-01-13 00:59:55 +0100

    debut inventory

---
+++ Assets/Scripts/inventory/Inventory.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    
+    public List<Item> content = new List<Item>();
+    public int currentItem = 0;
+
+}
