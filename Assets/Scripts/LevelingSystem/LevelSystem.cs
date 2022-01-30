b63ea02b74e2798a0a3821458a23a2d56467bbe2
Kerlann <kerlann.pro@gmail.com>
2022-01-30 14:51:58 +0100

    Système de level infini

--- Assets/Scripts/LevelingSystem/LevelSystem.cs
+++ Assets/Scripts/LevelingSystem/LevelSystem.cs
@@ -7,21 +7,16 @@ public class LevelSystem {
 
     public event EventHandler OnExperienceChanged;
     public event EventHandler OnLevelChanged;
-
-    private static readonly int[] experiencePerLevel = new[] { 100, 120, 140, 160, 180, 200, 220, 250, 300, 400, 500 };
-
     public LevelSystem() {}
 
     public void AddExperience(int amount) {
-        if (!IsMaxLevel()) {
-            PlayerData.getData().experience += amount;
-            while (!IsMaxLevel() && PlayerData.getData().experience >= GetExperienceToNextLevel(PlayerData.getData().level)) {
-                PlayerData.getData().experience -= GetExperienceToNextLevel(PlayerData.getData().level);
-                PlayerData.getData().level++;
-                if (OnLevelChanged != null) OnLevelChanged(this, EventArgs.Empty);
-            }
-            if (OnExperienceChanged != null) OnExperienceChanged(this, EventArgs.Empty);
+        PlayerData.getData().experience += amount;
+        while (PlayerData.getData().experience >= GetExperienceToNextLevel(PlayerData.getData().level)) {
+            PlayerData.getData().experience -= GetExperienceToNextLevel(PlayerData.getData().level);
+            PlayerData.getData().level++;
+            if (OnLevelChanged != null) OnLevelChanged(this, EventArgs.Empty);
         }
+        if (OnExperienceChanged != null) OnExperienceChanged(this, EventArgs.Empty);
     }
 
     public int GetLevelNumber() {
@@ -29,11 +24,7 @@ public class LevelSystem {
     }
 
     public float GetExperienceNormalized() {
-        if (IsMaxLevel()) {
-            return 1f;
-        } else {
-            return (float)PlayerData.getData().experience / GetExperienceToNextLevel(PlayerData.getData().level);
-        }
+        return (float)PlayerData.getData().experience / GetExperienceToNextLevel(PlayerData.getData().level);
     }
 
     public int GetExperience() {
@@ -41,20 +32,7 @@ public class LevelSystem {
     }
 
     public int GetExperienceToNextLevel(int level) {
-        if (level < experiencePerLevel.Length) {
-            return experiencePerLevel[level];
-        } else {
-            Debug.LogError("Level invalid: " + level);
-            return 100;
-        }
-    }
-
-    public bool IsMaxLevel() {
-        return IsMaxLevel(PlayerData.getData().level);
-    }
-
-    public bool IsMaxLevel(int level) {
-        return level == experiencePerLevel.Length - 1;
+        return (level * 100);
     }
 
 }
