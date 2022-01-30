0a56c648a58738f6702909b8f71186e17eca5a1a
Kerlann <kerlann.pro@gmail.com>
2022-01-30 22:05:10 +0100

    Update Enemy.cs

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,11 +4,11 @@ public class Enemy : MonoBehaviour
 {
     float health { get; set; }
 
-    public void ReceiveDommage(float damage){
+    public virtual void ReceiveDommage(float damage){
         this.health -= damage;
         if (health <= 0)
             onDie(); 
     }
 
-    public void onDie(){}
+    public virtual void onDie(){}
 }
