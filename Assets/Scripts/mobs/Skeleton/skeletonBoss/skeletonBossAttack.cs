9c5d3e272f0764fd426edb805341d578539a7822
IKEA-luddes <luddes@ikea.se>
2022-02-04 14:37:32 +0100

    add SkeletonBoss
    Starting SkeletonBoss

---
+++ Assets/Scripts/mobs/Skeleton/skeletonBoss/skeletonBossAttack.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skeletonBossAttack : MonoBehaviour, ISkeletonAttack
+{
+    public SkeletonMovement skeletonMovement;
+    public Animator animator;
+    public Rigidbody2D rb;
+
+    public void onAttack()
+    {
+        StartCoroutine(playAnimationAttack(1));
+    }
+
+    private IEnumerator playAnimationAttack(int number)
+    {
+        animator.Play("Skeleton_attack_" + number);
+        skeletonMovement.isAttack = true;
+        rb.velocity = new Vector2(0, 0);
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length + 0.2f);
+        animator.Play("Skeleton_idle");
+        skeletonMovement.isAttack = false;
+    }
+}
