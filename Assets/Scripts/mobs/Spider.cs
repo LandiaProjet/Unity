5c96879f5bd252ff902701dbdeb6c6f9b065f7a9
Kerlann <kerlann.pro@gmail.com>
2022-02-05 21:42:50 +0100

    Spider

--- Assets/Scripts/mobs/Spider.cs
+++ Assets/Scripts/mobs/Spider.cs
@@ -15,29 +15,42 @@ public class Spider : Enemy
  
     private Rigidbody2D rb;
 
+    public float damage = 1f;
+
+    public BoxCollider2D boxCollider2D;
+
+
+    private bool cooling;
+    private float intTimer;
+    public float timer;
  
     void Awake() {
         rb = gameObject.GetComponent<Rigidbody2D>();
         anim = gameObject.GetComponent<Animator>();
+
+        intTimer = timer;
     }
  
     private void FixedUpdate() {
+        if (cooling)
+        {
+            Cooldown();
+            anim.SetBool("Attack", false);
+        }
         if (hasTarget) {
             float distance = Vector3.Distance(transform.position, target.transform.position);
             if (distance > 2) {
                 transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
                 anim.SetBool("canWalk", true);
-                anim.SetBool("Attack", false);
+                StopAttack();
             } else {
                 anim.SetBool("canWalk", false);
-                anim.SetBool("Attack", true);
+                if(!cooling)
+                    Attack();
             }
-            if(target.transform.position.x > transform.position.x){
-                transform.localScale = new Vector3(1,1,1);
-            }else if(target.transform.position.x < transform.position.x){
-                transform.localScale = new Vector3(-1,1,1);
-            }
+            Flip();
         }
+    
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
@@ -46,14 +59,54 @@ public class Spider : Enemy
             hasTarget = true;   
         }
     }
- 
- 
-    private void OnTriggerExit2D(Collider2D collision) {
-        if (collision.CompareTag("Player")) {
-            target = null;
-            hasTarget = false;
+
+
+    void Flip()
+    {
+        if (target.transform.position.x > transform.position.x)
+        {
+            transform.localScale = new Vector2(1, transform.localScale.y);
+        }
+        else if (target.transform.position.x < transform.position.x)
+        {
+            transform.localScale = new Vector2(-1, transform.localScale.y);
         }
     }
- 
+    
+    void Cooldown()
+    {
+        timer -= Time.deltaTime;
 
+        if (timer <= 0 && cooling)
+        {
+            cooling = false;
+            timer = intTimer;
+        }
+    }
+
+    private void StopAttack()
+    {
+        cooling = false;
+        anim.SetBool("Attack", false);
+    }
+    
+    private void Attack()
+    {
+        timer = intTimer;
+        
+        anim.SetBool("Attack", true);
+    }
+
+    private void Damage(){
+        if(boxCollider2D.IsTouching(target.GetComponent<Collider2D>())){
+            ReceiveDommage(damage);
+            Debug.Log("damage !");
+        }
+    }
+
+    public void TriggerCooling()
+    {
+        cooling = true;
+    }
+    
 }
