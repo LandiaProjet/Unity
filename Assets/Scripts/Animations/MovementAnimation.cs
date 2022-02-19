e61e7ee97870656c2c2c6d3f21b4b16fc046bbf9
ender <ender@lol.com>
2022-02-19 15:51:30 +0100

    add MovementAnimation & RotationAnimation

---
+++ Assets/Scripts/Animations/MovementAnimation.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementAnimation : MonoBehaviour
+{
+    public float speed;
+    public Transform[] waypoints;
+
+    private Transform target;
+    private int destPoint;
+
+    private void OnEnable()
+    {
+        target = waypoints[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.localPosition - transform.localPosition;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.localPosition, target.localPosition) < 0.3f)
+        {
+            destPoint = (destPoint + 1) % waypoints.Length;
+            target = waypoints[destPoint];
+        }
+    }
+}
---
+++ Assets/Scripts/Animations/RotationAnimation.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationAnimation : MonoBehaviour
+{
+    public float speed;
+    public float Angle;
+
+    private Vector3 eulerAngles;
+
+    private void OnEnable()
+    {
+        eulerAngles = transform.eulerAngles;
+    }
+
+    void Update()
+    {
+        Vector3 dir = new Vector3(0, 0, Angle);
+        transform.Rotate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        Vector3 finalPosition = eulerAngles + dir;
+        if (Mathf.Abs(Quaternion.Angle(transform.rotation, Quaternion.Euler(finalPosition.x, finalPosition.y, finalPosition.z))) < 1.0f)
+        {
+            Angle *= -1;
+        }
+    }
+}
